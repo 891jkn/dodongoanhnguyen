3a6fff77614801633ab0ec1c867c5a7e9327e5d8
khanhdaica <dangk1312200221@gmail.com>
2022-05-16 00:42:01 +0700

    finish basis home

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,21 @@
 let navbarTogglerBtn = document.getElementById("navbar-toggler-btn")
 let navbar = document.querySelector(".main-nav")
 let closer = document.getElementById("toggler-close-btn")
+let subNav = document.querySelector(".banner-wrap")
 let togller = 0;
 closer.addEventListener('click',(e)=>{
     togller = 0
     if(navbar.classList.contains("show-main-nav")){
         navbarTogglerBtn.classList.remove("btn-toggler-show")
         navbar.classList.remove("show-main-nav")
+        subNav.classList.add('sticky-top')
     }
 })
 
 window.onresize = (e)=>{
     if(window.screen.width >= 768){
         togller = 0
+        subNav.classList.remove('sticky-top')
         if(navbar.classList.contains("show-main-nav")){
             navbarTogglerBtn.classList.remove("btn-toggler-show")
             navbar.classList.remove("show-main-nav")
@@ -21,28 +24,43 @@ window.onresize = (e)=>{
 }
 
 const handleToggler = (event)=>{
-    console.log(event.target.parent)
     if(togller == 0){
         togller = 1
         if(!navbar.classList.contains("show-main-nav")){
             navbarTogglerBtn.classList.add("btn-toggler-show")
             navbar.classList.add("show-main-nav")
+            subNav.classList.remove('sticky-top')
         }
     }else{
         if(navbar.classList.contains("show-main-nav")){
             togller = 0
             navbarTogglerBtn.classList.remove("btn-toggler-show")
             navbar.classList.remove("show-main-nav")
+            subNav.classList.remove('sticky-top')
         }
     }
 }
 
 window.addEventListener('scroll',(e)=>{
-    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 20){
-        navbar.classList.add('sticky-top')
+    if(window.screen.width > 767){
+        if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 20){
+            navbar.classList.add('sticky-top')
+        }else{
+            navbar.classList.remove('sticky-top')
+        }
     }else{
-        navbar.classList.remove('sticky-top')
+        if(!togller){
+            if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 20){
+                subNav.classList.add('sticky-top')
+            }else{
+                subNav.classList.remove('sticky-top')
+            }
+        }else{
+            subNav.classList.remove('sticky-top')
+        }
+       
     }
+   
 })
 navbarTogglerBtn.addEventListener('click',handleToggler)
 
